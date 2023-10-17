8277dcc1027a175ad2953f69565b4cddd13ef0c5
Lika-25 <kapiba@ukr.net>
2023-10-18 01:00:22 +0300

    Налагодження роботи з формою

--- mainProject/mainProject/MyForm.cpp
+++ mainProject/mainProject/MyForm.cpp
@@ -1,2 +1,14 @@
 #include "MyForm.h"
 
+
+using namespace System;
+using namespace System::Windows::Forms;
+int barrel;
+[STAThreadAttribute]
+int main(cli::array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    mainProject::MyForm form;
+    Application::Run(% form);
+    return 0;
+}
--- mainProject/mainProject/MyForm.h
+++ mainProject/mainProject/MyForm.h
@@ -10,7 +10,7 @@ namespace mainProject {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// —водка дл€ MyForm
+	/// Summary for MyForm
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
@@ -19,13 +19,13 @@ namespace mainProject {
 		{
 			InitializeComponent();
 			//
-			//TODO: добавьте код конструктора
+			//TODO: Add the constructor code here
 			//
 		}
 
 	protected:
 		/// <summary>
-		/// ќсвободить все используемые ресурсы.
+		/// Clean up any resources being used.
 		/// </summary>
 		~MyForm()
 		{
@@ -34,26 +34,48 @@ namespace mainProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
-		/// ќб€зательна€ переменна€ конструктора.
+		/// Required designer variable.
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
-		/// содержимое этого метода с помощью редактора кода.
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(214, 107);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(161, 113);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(560, 459);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
